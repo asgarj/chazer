aebcdb977cf293ebf31458e18204a21b200dcace
asgarJ <esger.javadov@gmail.com>
2017-04-03 09:29:10 +0400

    refactor

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,18 +1,15 @@
 'use strict';
 
 function generate_table(nRows, nCols) {
-	console.log(nRows + " " + nCols);
 	var board = document.getElementById("board");
-	console.log(board);
 	for (var i = 1; i<= nRows; i++) {
 		var row = board.insertRow(i-1);
+		row.className = "height";
 		generateRow(row, i, nCols);
-		console.log(row);
 	}
 }
 
 function generateRow(row, rowId, nCols) {
-	row.className = "height";
 	for (var i=0; i<nCols; i++) {
 		var cell = row.insertCell(i);
 		cell.id = rowId + ":" + (i+1);
